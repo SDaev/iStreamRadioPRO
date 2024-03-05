c834e38c1c4beef392b2864bfb271efc19af476a
Skorka Dávid <145161454+SDaev@users.noreply.github.com>
2024-03-05 22:03:46 +0100

    base.html létrehozva

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, world!'
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
