0b537e2cb6eb041dd684dbc057e81872d5b14c2f
Skorka Dávid <145161454+SDaev@users.noreply.github.com>
2024-03-09 12:43:17 +0100

    Index lap és db teszt adatokkal

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import base64
 import sqlite3
 
 app = Flask(__name__)
@@ -12,7 +13,18 @@ con.close()
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    con = sqlite3.connect(db)
+    c = con.cursor()
+
+    radiok = []
+    for r in c.execute("select * from radiok"):
+        logo_bytes = bytes.fromhex(r[3])
+        logo_b64 = base64.b64encode(logo_bytes).decode()
+        radiok.append(
+            {"name": r[0], "frequency": r[1], "description": r[2], "logo": logo_b64}
+        )
+    con.close()
+    return render_template("index.html", radiok=radiok)
 
 
 if __name__ == "__main__":
