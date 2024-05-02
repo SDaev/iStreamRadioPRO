6f34c79ccc3bc3538e7a58bcb4fb89e862c92265
Skorka Dávid <145161454+SDaev@users.noreply.github.com>
2024-05-02 21:27:11 +0200

    Működik

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template
-import base64
 import sqlite3
+
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 db = "istreamradio.sqlite"
-
 con = sqlite3.connect(db)
 c = con.cursor()
-con.close()
-
-nowplaying = 0
 
 
 @app.route("/")
@@ -18,18 +14,67 @@ def index():
     con = sqlite3.connect(db)
     c = con.cursor()
 
-    nowplaying == 0
     radiok = []
     for r in c.execute("select * from radiok"):
-        radiok.append({"id": r[0], "name": r[1], "url": r[2], "logo": r[3]})
+        radiok.append(
+            {
+                "id": r[0],
+                "name": r[1],
+                "url": r[2],
+                "logo_thumbnail": r[3],
+                "logo_large": r[4],
+            }
+        )
+
+    c.execute("select * from radiok where id = 0")
+    r = c.fetchone()
+
     con.close()
-    return render_template("index.html", radiok=radiok)
+    return render_template(
+        "index.html",
+        radiok=radiok,
+        radio={
+            "id": r[0],
+            "name": r[1],
+            "url": r[2],
+            "logo_thumbnail": r[3],
+            "logo_large": r[4],
+        },
+    )
 
 
-@app.route("/<int:id>", methods=["POST"])
-def play(id):
-    pass
-    nowplaying == id
+@app.route("/lejatszas/<int:id>")
+def lejatszas(id):
+    con = sqlite3.connect(db)
+    c = con.cursor()
+
+    radiok = []
+    for r in c.execute("select * from radiok"):
+        radiok.append(
+            {
+                "id": r[0],
+                "name": r[1],
+                "url": r[2],
+                "logo_thumbnail": r[3],
+                "logo_large": r[4],
+            }
+        )
+
+    c.execute("select * from radiok where id = ?", [id])
+    r = c.fetchone()
+
+    con.close()
+    return render_template(
+        "index.html",
+        radiok=radiok,
+        radio={
+            "id": r[0],
+            "name": r[1],
+            "url": r[2],
+            "logo_thumbnail": r[3],
+            "logo_large": r[4],
+        },
+    )
 
 
 if __name__ == "__main__":
